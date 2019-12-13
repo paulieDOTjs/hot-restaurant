e262939806cfbb8f388c4267a01dda9a69e6b0ba
Paul Hendrickson <PaulRobHendrickson@gmail.com>
2019-12-13 14:09:34 -0600

    creating repot

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const path = require("path")
+
+const app = express();
+const PORT = 3000;
+
+const reservations = [];
+const waitList = [];
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "makeReservation.html"));
+});
+
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "table.html"));
+});
+
+app.get("/api/waitlist", function (req, res) {
+    res.sendFile(path.join(__dirname, "waitListApi.html"));
+    res.json(waitList);
+});
+
+app.get("/api/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "makeTableApi.html"));
+    res.json(reservations);
+});
+
+app.post("/api/tables", function (req, res) {
+    const reservationRequest = req.body;
+    if (reservations.length > 4) {
+        console.log('waitlist')
+        waitList.push(reservationRequest);
+    } else {
+        console.log('reservation')
+        reservations.push(reservationRequest);
+    }
+})
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
